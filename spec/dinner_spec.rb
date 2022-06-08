4ccb0324bb1e1a94b1bc69fc24070232b8c6ff22
LJMas <lucy.massie@madetech.com>
2022-06-08 14:55:16 +0100

    Initial commit

---
+++ spec/dinner_spec.rb
@@ -0,0 +1,16 @@
+# Dinner must be: Delicious
+#           - Good balance of flavour
+# Dinner must be: Nutritious
+#           - Should provide at least 50% of RDA vitamins
+#           - Should not exceed daily limits of bad stuff
+# Dinner must be: Conscientious
+#           - Ingredients sourced from approved suppliers
+# Dinner must be: Delivered-to-us
+#           - Maximum 30 mins from ordering to delivery
+
+describe 'my dinner' do
+    it 'is delicious' do
+      #my_dinner = Dinner.new
+      expect(1+1).to eq(3)
+    end  
+  end
